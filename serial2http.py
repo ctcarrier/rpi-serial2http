0b81da46de9edb9c2624bc4232b5e2de4ff7444c
ctcarrier <ctcarrier@gmail.com>
2016-09-29 22:26:00 -0400

    Parsing manually

--- serial2http.py
+++ serial2http.py
@@ -1,6 +1,11 @@
 import serial
+from xbee import XBee
 
-ser = serial.Serial('/dev/ttyUSB0')
+ser = serial.Serial('/dev/ttyAMA0')
+xbee = XBee(ser)
 
 while True:
-    rcv = ser.read(10)
+    rcv = ser.read(21)
+    print(str(rcv).encode('hex'))
+    parsed = xbee._split_response(rcv)
+    print(parsed)
