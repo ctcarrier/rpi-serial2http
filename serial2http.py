e3dbbb54062b6ab635c8cafe081ab6e1864d0b7b
ctcarrier <ctcarrier@gmail.com>
2016-09-30 21:21:16 -0400

    Improve

--- serial2http.py
+++ serial2http.py
@@ -5,7 +5,12 @@ ser = serial.Serial('/dev/ttyAMA0')
 xbee = XBee(ser)
 
 while True:
+    frame = APIFrame(escaped=self._escaped)
     rcv = ser.read(21)
     print(str(rcv).encode('hex'))
-    parsed = xbee._split_response(rcv)
+    for b in rcv:
+        frame.fill(b)
+
+    frame.parse()
+    parsed = xbee._split_response(frame)
     print(parsed)
