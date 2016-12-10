d26e99dcfebd3a766d9db26cd265a7e4a9a2a946
ctcarrier <ctcarrier@gmail.com>
2016-12-10 16:54:51 -0500

    Debug script for xbee

---
+++ xbeedebug.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import serial
+from xbee import XBee
+import requests
+import datetime
+import os
+import logging
+
+serial_port = serial.Serial('/dev/ttyAMA0', 9600)
+xbee = XBee(ser = serial_port, escaped = True)
+
+LOG_FILENAME = '/var/log/xbeedebug.log'
+
+logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO, format='%(asctime)s %(message)s')
+logging.info('Started %s' % __file__)
+
+while True:
+    try:
+        data = xbee.wait_read_frame()
+        data_split = data['rf_data'].split(',', 2)
+        logging.info(data['rf_data'])
+        print(data['rf_data'])
+    except KeyboardInterrupt:
+        break
+
+serial_port.close()
